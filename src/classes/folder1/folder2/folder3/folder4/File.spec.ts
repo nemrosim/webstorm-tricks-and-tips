3b42f36db64e3cac0efdbfd29b329efea423e75d
nemrosim <nemrosim1988@gmail.com>
2021-03-28 18:04:00 +0300

    add new files

---
+++ src/classes/folder1/folder2/folder3/folder4/File.spec.ts
@@ -0,0 +1,6 @@
+import { File } from './File';
+
+test('should do something', () => {
+    const result = File.sum(2, 2);
+    expect(result).toEqual(4);
+});
---
+++ src/classes/folder1/folder2/folder3/folder4/File.ts
@@ -0,0 +1,10 @@
+export class File {
+    static sum = (a: number, b: number) => {
+        return a + b;
+    }
+
+    static double = (a: number, b: number) => {
+        return a * b;
+    }
+}
+
